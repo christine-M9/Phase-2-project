7fa3e12a9f3f1c4d477defc1d421027367107267
Christine <christinekhekama99@gmail.com>
2023-08-04 13:17:08 +0300

    comments to PopDataSearch

--- population/src/PopulationDataSearch.js
+++ population/src/PopulationDataSearch.js
@@ -1,16 +1,19 @@
+// importing dependencies
 import React, { useEffect, useState } from 'react';
 import { Box, Typography, Button } from '@mui/material';
 
+// functional component
 const PopulationDataSearch = ({ onEnlist }) => {
   const [populationData, setPopulationData] = React.useState([]);
   const [searchYear, setSearchYear] = React.useState('');
   const [filteredData, setFilteredData] = React.useState([]);
 
+// use effect hook
   useEffect(() => {
     fetchPopulationData();
   }, []);
 
-  // GET request
+// GET request
   const fetchPopulationData = () => {
     fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
       .then((response) => response.json())
@@ -22,6 +25,7 @@ const PopulationDataSearch = ({ onEnlist }) => {
       });
   };
 
+  // filtering populationdata
   useEffect(() => {
     const filteredResults = populationData.filter(
       (item) => item.Year.toString() === searchYear
@@ -29,6 +33,7 @@ const PopulationDataSearch = ({ onEnlist }) => {
     setFilteredData(searchYear === '' ? populationData : filteredResults);
   }, [searchYear, populationData]);
 
+  // input field
   return (
     <Box>
       <input
@@ -37,6 +42,8 @@ const PopulationDataSearch = ({ onEnlist }) => {
         value={searchYear}
         onChange={(e) => setSearchYear(e.target.value)}
       />
+
+ {/* mapping filtered data and enlisting */}
       {filteredData.map((item) => (
         <Box key={item["ID Year"]} sx={{ border: 1, p: 2, my: 2, cursor: 'pointer', backgroundColor: '#f0f0f0' }} onClick={() => onEnlist(item)}>
           <Typography variant="h6">ID Year: {item["ID Year"]}</Typography>
@@ -50,5 +57,5 @@ const PopulationDataSearch = ({ onEnlist }) => {
     </Box>
   );
 };
-
+// default export
 export default PopulationDataSearch;
